10d12e9107a7758674805a516be37e7498b5bc05
Andy Leitermann <andyleitermann@gmail.com>
2014-05-21 17:21:13 -0600

    updating bfi.py

--- bfi.py
+++ bfi.py
@@ -8,7 +8,7 @@ print sys.argv
 
 commands = ""
 turing = [0]
-
+pos = 0
 
 if len(sys.argv) > 1:
     commands = sys.argv[1]
@@ -18,22 +18,34 @@ if commands:
 
 for i in commands:
     if i == "+":
+        turing[pos] += 1
         print i
     elif i == "-":
+        turing[pos] -= 1
         print i
     elif i == ">":
+        if len(turing)-1 == pos:
+            turing.append(0)
+        pos += 1
         print i
     elif i == "<":
+        if pos != 0:
+            pos -= 1
+        else:
+            print "already at position 0"
         print i
     elif i == "[":
         print i
     elif i == "]":
         print i
     elif i == ".":
+        print chr(turing[pos])
         print i
     elif i == ",":
         print i
     else: 
         print "what's this "+i+" of which you speak?"
+    print turing
+    print "position: "+str(pos)
 
 print "success!"

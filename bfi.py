7223818faf7d86585126c971a819ea1c90795045
Andy Leitermann <andyleitermann@gmail.com>
2014-05-22 09:54:51 -0600

    switched for loop to while for control structures

--- bfi.py
+++ bfi.py
@@ -14,7 +14,7 @@ commands = ""
 turing = [0]
 pos = 0
 x = 0
-
+iter = 0
 
 if len(sys.argv) > 1:
     commands = sys.argv[1]
@@ -22,44 +22,46 @@ if len(sys.argv) > 1:
 if commands:
     print commands
 
-for i in commands:
-    if i == "+":
+while iter < len(commands):
+#for i in commands:
+    if commands[iter] == "+":
         turing[pos] += 1
-        print i
-    elif i == "-":
+        print commands[iter]
+    elif commands[iter] == "-":
         turing[pos] -= 1
-        print i
-    elif i == ">":
+        print commands[iter]
+    elif commands[iter] == ">":
         if len(turing)-1 == pos:
             turing.append(0)
         pos += 1
-        print i
-    elif i == "<":
+        print commands[iter]
+    elif commands[iter] == "<":
         if pos != 0:
             pos -= 1
         else:
             print "already at position 0"
-        print i
-    elif i == "[":
+        print commands[iter]
+    elif commands[iter] == "[":
         # this is a tough one, might change main for loop to while
-        print i
-    elif i == "]":
+        print commands[iter]
+    elif commands[iter] == "]":
         # similar to [ of course
-        print i
-    elif i == ".":
+        print commands[iter]
+    elif commands[iter] == ".":
         if turing[pos] >= 0:
             print chr(turing[pos])
         else:
             print "cannot display negative number as char"
-        print i
-    elif i == ",":
+        print commands[iter]
+    elif commands[iter] == ",":
         # type a char and press enter, may fix for direct from stdin later.
         x = ord(raw_input())
         turing[pos] = x
-        print i
+        print commands[iter]
     else: 
-        print "what's this "+i+" of which you speak?"
+        print "what's this "+commands[iter]+" of which you speak?"
     print turing
     print "position: "+str(pos)
+    iter += 1
 
 print "success!"
